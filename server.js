fc3294c35aade317b636945006eba92d76c1f25d
Elankathir <esekar@diligentsquad.com>
2025-11-01 07:53:35 +0530

    handling rejected promises, uncaught exceptions and debuging code

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const dotenv = require("dotenv"); //install dotenv
 dotenv.config({ path: "./config.env" }); //set user defined env variables to environment
 
 const mongoose = require("mongoose");
+
+//handel unCaught error
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Un-Caught error occurred, shutting down");
+
+  process.exit(1);
+});
+
+//note: reason for placing it before "app" to liston Un-Caught error widely, need to listen before they occurred
+
 const app = require("./app");
 
 //console.log(process.env);
@@ -11,14 +22,11 @@ mongoose
   .then((conn) => {
     //console.log(conn);
     console.log("DB Connection Successful");
-  })
-  .catch((error) => {
-    console.log("Some Error has Occured");
   });
 
 //Create Server
 const port = process.env.PORT || 3000; //accessing environment variables
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log("Server has started");
 });
 
@@ -27,3 +35,15 @@ app.listen(port, () => {
 //app.listen(port, "10.3.1.143", () => {
 //  console.log(`Server has started on http://10.3.1.143:${port}`);
 //});
+
+//console.log(x);
+
+//handel rejection error
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection error occurred, shutting down");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
