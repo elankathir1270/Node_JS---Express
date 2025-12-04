2ef1df7bc9d14c244f108a99a85da417033f5de3
Elankathir <esekar@diligentsquad.com>
2025-12-04 11:37:55 +0530

    security http headers data sanitization prevent param pollution

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //Import package
 const express = require("express"); //returns function
 const morgan = require("morgan"); // to get request details
-const rateLimit = require("express-rate-limit");
 const qs = require("qs");
+
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const sanitize = require("mongo-sanitize");
+const sanitizeHtml = require("sanitize-html");
+const hpp = require("hpp"); //http parameter pollution
+
 const moviesRouter = require("./routes/moviesRoutes");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -10,6 +16,8 @@ const CustomError = require("./utils/customError");
 const globalErrorHandler = require("./controller/errorController");
 
 const app = express(); //returns object
+
+app.use(helmet()); //this will add some security header to response
 
 const limiter = rateLimit({
   max: 10, //max requests
@@ -20,7 +28,52 @@ const limiter = rateLimit({
 
 app.use("/api", limiter); //to all api which has '/api'.
 
+app.use(express.json()); //middleware //returns middleware function
+app.use(express.urlencoded({ extended: true }));
+
+/*
+app.use(express.json({ limit: "10kb" }));
+now it will only accept max of '10kb' in the request body
+*/
+
 app.set("query parser", (str) => qs.parse(str));
+
+app.use(
+  hpp({
+    whitelist: ["prize", "duration"], //not working for sort
+  })
+);
+
+//sanitize
+app.use((req, res, next) => {
+  req.body = sanitize(req.body);
+  req.query = sanitize(req.query);
+  req.params = sanitize(req.params);
+  next();
+});
+//this is to send clean request data without any malicious characters and patterns before the data is used in your application
+
+//app.use(xss()); //this will prevent any malicious html code from input(the package is outdated so,cannot use it with Express 5)
+//sanitizeHtml
+app.use((req, res, next) => {
+  const clean = (value) => {
+    if (typeof value === "string") {
+      return sanitizeHtml(value);
+    }
+    if (typeof value === "object" && value !== null) {
+      for (let key in value) {
+        value[key] = clean(value[key]);
+      }
+    }
+    return value;
+  };
+
+  req.body = clean(req.body);
+  req.query = clean(req.query);
+  req.params = clean(req.params);
+
+  next();
+});
 
 //creating custom middleware
 const logger = function (req, res, next) {
@@ -29,7 +82,6 @@ const logger = function (req, res, next) {
   next(); //logger function will not be executed unless next() function is called.
 };
 
-app.use(express.json()); //middleware //returns middleware function
 app.use(logger); // already in middleware function format so we didn't use'()'
 app.use(express.static("./public")); //serving static files
 if (process.env.NODE_ENV === "development") {
